214b1ce736da43b85a3ed4166ad91dbb04416ea1
Dedenneee <st.na.fm@gmail.com>
2021-02-08 23:12:04 +0900

    最初のコミット

---
+++ src/main/java/net/kunmc/lab/rinnetensei/RinneTenseiCraft.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.rinnetensei;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RinneTenseiCraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
