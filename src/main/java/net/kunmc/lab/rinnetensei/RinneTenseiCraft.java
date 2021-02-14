f9afb381ba840e37ffd02ee7151839cdb221d4dc
Dedenneee <st.na.fm@gmail.com>
2021-02-15 01:05:00 +0900

    完成

--- src/main/java/net/kunmc/lab/rinnetensei/RinneTenseiCraft.java
+++ src/main/java/net/kunmc/lab/rinnetensei/RinneTenseiCraft.java
@@ -1,17 +1,138 @@
 package net.kunmc.lab.rinnetensei;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class RinneTenseiCraft extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        TenseiListener.evolveMap = new HashMap<EntityType, EntityType>(){
+            {
+                put(EntityType.BEE, EntityType.PHANTOM);
+                put(EntityType.BAT, EntityType.PHANTOM);
+                put(EntityType.CHICKEN, EntityType.PHANTOM);
+                put(EntityType.PARROT, EntityType.PHANTOM);
+                put(EntityType.PHANTOM, EntityType.GHAST);
+                put(EntityType.GHAST, EntityType.ENDER_DRAGON);
+                put(EntityType.ENDER_DRAGON, EntityType.CHICKEN);
 
+                put(EntityType.COW, EntityType.MUSHROOM_COW);
+                put(EntityType.MUSHROOM_COW, EntityType.RAVAGER);
+                put(EntityType.RAVAGER, EntityType.COW);
+
+                put(EntityType.SQUID, EntityType.PUFFERFISH);
+                put(EntityType.DOLPHIN, EntityType.PUFFERFISH);
+                put(EntityType.TURTLE, EntityType.PUFFERFISH);
+                put(EntityType.SALMON, EntityType.PUFFERFISH);
+                put(EntityType.COD, EntityType.PUFFERFISH);
+                put(EntityType.TROPICAL_FISH, EntityType.PUFFERFISH);
+                put(EntityType.PUFFERFISH, EntityType.DROWNED);
+                put(EntityType.DROWNED, EntityType.GUARDIAN);
+                put(EntityType.GUARDIAN, EntityType.ELDER_GUARDIAN);
+                put(EntityType.ELDER_GUARDIAN, EntityType.SQUID);
+
+                put(EntityType.PIG, EntityType.CREEPER);
+                put(EntityType.CREEPER, EntityType.ENDERMAN);
+                put(EntityType.ENDERMAN, EntityType.ENDER_DRAGON);
+                put(EntityType.HORSE, EntityType.SKELETON_HORSE);
+                put(EntityType.DONKEY, EntityType.SKELETON_HORSE);
+                put(EntityType.MULE, EntityType.SKELETON_HORSE);
+                put(EntityType.SKELETON_HORSE, EntityType.PIG);
+
+                put(EntityType.SHEEP, EntityType.SKELETON);
+                put(EntityType.SKELETON, EntityType.WITHER_SKELETON);
+                put(EntityType.PIG_ZOMBIE, EntityType.WITHER_SKELETON);
+                put(EntityType.WITHER_SKELETON, EntityType.BLAZE);
+                put(EntityType.BLAZE, EntityType.WITHER);
+                put(EntityType.WITHER, EntityType.SHEEP);
+
+                put(EntityType.LLAMA, EntityType.SPIDER);
+                put(EntityType.PANDA, EntityType.SPIDER);
+                put(EntityType.POLAR_BEAR, EntityType.SPIDER);
+                put(EntityType.SPIDER, EntityType.CAVE_SPIDER);
+                put(EntityType.CAVE_SPIDER, EntityType.IRON_GOLEM);
+                put(EntityType.IRON_GOLEM, EntityType.LLAMA);
+
+                put(EntityType.CAT, EntityType.SLIME);
+                put(EntityType.OCELOT, EntityType.SLIME);
+                put(EntityType.FOX, EntityType.SLIME);
+                put(EntityType.RABBIT, EntityType.SLIME);
+                put(EntityType.SLIME, EntityType.MAGMA_CUBE);
+                put(EntityType.MAGMA_CUBE, EntityType.VEX);
+
+                put(EntityType.SILVERFISH, EntityType.ENDERMITE);
+                put(EntityType.ENDERMITE, EntityType.SHULKER);
+                put(EntityType.SHULKER, EntityType.SILVERFISH);
+
+                put(EntityType.STRAY, EntityType.HUSK);
+                put(EntityType.HUSK, EntityType.STRAY);
+
+                put(EntityType.WANDERING_TRADER, EntityType.PILLAGER);
+                put(EntityType.VILLAGER, EntityType.PILLAGER);
+                put(EntityType.PILLAGER, EntityType.VINDICATOR);
+                put(EntityType.VINDICATOR, EntityType.WITCH);
+                put(EntityType.WITCH, EntityType.EVOKER);
+                put(EntityType.EVOKER, EntityType.VILLAGER);
+
+                put(EntityType.ILLUSIONER, EntityType.VILLAGER);
+            }
+        };
+        getServer().getPluginManager().registerEvents(new TenseiListener(), this);
+        getLogger().info("輪廻転生プラグインが有効になりました");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("輪廻転生プラグインが無効になりました");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        if(!cmd.getName().equalsIgnoreCase("rinnetensei")){
+            return false;
+        }
+        if(args.length < 1){
+            sender.sendMessage("引数が足りません");
+            return false;
+        }else if(args.length > 1){
+            sender.sendMessage("引数が多すぎます");
+            return false;
+        }
+        String arg1 = args[0];
+
+        if(arg1.equalsIgnoreCase("on")){
+            TenseiListener.isEnabled = true;
+            sender.sendMessage("輪廻転生プラグインをオンにしました");
+            return true;
+        } else if(arg1.equalsIgnoreCase("off")){
+            TenseiListener.isEnabled = false;
+            sender.sendMessage("輪廻転生プラグインをオフにしました");
+            return true;
+        } else {
+            sender.sendMessage("「on」または「off」と入力してください");
+            return false;
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        switch (args.length) {
+            case 1:
+                return Stream.of("on", "off")
+                        .filter(e -> e.startsWith(args[0]))
+                        .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
---
+++ src/main/java/net/kunmc/lab/rinnetensei/TenseiListener.java
@@ -0,0 +1,102 @@
+package net.kunmc.lab.rinnetensei;
+
+import org.bukkit.Location;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.SlimeSplitEvent;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class TenseiListener implements Listener {
+
+    public static HashMap<EntityType, EntityType> evolveMap;
+    public static boolean isEnabled = false;
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e){
+        if(!isEnabled){
+            return;
+        }
+
+        Entity entity = e.getEntity();
+        Location loc = entity.getLocation();
+        EntityType entityType = entity.getType();
+
+        if(entity instanceof Player){
+            return;
+        }
+
+        Random random = new Random();
+        int num = random.nextInt(20);
+        if(num == 0){
+            loc.getWorld().spawnEntity(loc, EntityType.ILLUSIONER);
+        }
+
+        // ヴェックスの場合
+        if(entityType.equals(EntityType.VEX)){
+            if(num <= 4){
+                loc.getWorld().spawnEntity(loc, EntityType.PIG);
+            }else if(num <= 9){
+                loc.getWorld().spawnEntity(loc, EntityType.COW);
+            }else if(num <= 14){
+                loc.getWorld().spawnEntity(loc, EntityType.SHEEP);
+            }else{
+                loc.getWorld().spawnEntity(loc, EntityType.CHICKEN);
+            }
+            return;
+        }
+
+        // ブタの場合
+        if(entityType.equals(EntityType.PIG)){
+            if(entity.getLastDamageCause().getCause().equals((EntityDamageEvent.DamageCause.FIRE)) ||
+                    entity.getLastDamageCause().getCause().equals((EntityDamageEvent.DamageCause.FIRE_TICK))){
+                PigZombie zombiePig = (PigZombie) loc.getWorld().spawnEntity(loc, EntityType.PIG_ZOMBIE);
+                zombiePig.setAngry(true);
+                return;
+            }
+        }
+        // クリーパーの場合
+        if(entityType.equals(EntityType.CREEPER)){
+            Creeper creeper = (Creeper) entity;
+            if(creeper.isPowered()){
+                SkeletonHorse horse = (SkeletonHorse) loc.getWorld().spawnEntity(loc, EntityType.SKELETON_HORSE);
+                horse.setTrap(true);
+                return;
+            }
+            if(entity.getLastDamageCause().getCause().equals((EntityDamageEvent.DamageCause.ENTITY_EXPLOSION))){
+               Creeper creeper2 = (Creeper) loc.getWorld().spawnEntity(loc, EntityType.CREEPER);
+               creeper2.setPowered(true);
+               return;
+            }
+        }
+
+        Entity spawnEntity = loc.getWorld().spawnEntity(loc, evolveMap.get(entity.getType()));
+
+        // ボスのヘルス調整
+        if(evolveMap.get(entity.getType()).equals(EntityType.WITHER) ){
+            Wither wither = (Wither) spawnEntity;
+            wither.setHealth(100D);
+        }
+        if(evolveMap.get(entity.getType()).equals(EntityType.ENDER_DRAGON) ){
+            EnderDragon dragon = (EnderDragon) spawnEntity;
+            dragon.setHealth(100D);
+        }
+    }
+
+    @EventHandler
+    public void onSlimeSplit(SlimeSplitEvent e){
+        if(!isEnabled){
+            return;
+        }
+        e.setCancelled(true);
+    }
+
+    public EntityType evolveEntity(EntityType entityType){
+        return EntityType.ZOMBIE;
+    }
+
+}
